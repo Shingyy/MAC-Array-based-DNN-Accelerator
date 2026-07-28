6a6695e728f61b14c65b130245c3182b2fe90da2
Ryan Kusekwa <146185991+Shingyy@users.noreply.github.com>
2026-07-28 13:47:48 +0300

    Update fifo_tb.vhd
    Added a clock divider to generate the read and write enable tick signals  for the Async FIFO since it now uses one main clock.

--- Activation FIFO/sim/fifo_tb.vhd
+++ Activation FIFO/sim/fifo_tb.vhd
@@ -6,13 +6,12 @@ entity fifo_tb is
 end entity fifo_tb;
 
 architecture rtl of fifo_tb is
-    signal CLK_A, CLK_B,RESET, EMPTY, FULL, ALMOST_EMPTY, ALMOST_FULL, WRE, RE: std_logic;
+    signal CLK,RESET, EMPTY, FULL, ALMOST_EMPTY, ALMOST_FULL,WRE_TICK, RE_TICK, WRE, RE: std_logic;
     signal D_IN, D_OUT: signed(7 downto 0);
 begin
     my_fifo_obj: entity work.fifo
     port map(
-        CLK_A=> CLK_A,
-        CLK_B=> CLK_B,
+        CLK=> CLK,
         EMPTY=> EMPTY,
         FULL=> FULL,
         RESET=> RESET,
@@ -20,10 +19,22 @@ begin
         ALMOST_FULL=> ALMOST_FULL,
         WRE=> WRE,
         RE=> RE,
+        RE_IICK=> RE_TICK,
+        WRE_TICK=>WRE_TICK,
         D_IN=> D_IN,
         D_OUT=> D_OUT
     );
-    --CLOCK A: 100 MHz clock
+    clock_divider: entity work.clock_divider
+    generic map(
+        WRITE_CYCLES=> 1, --100MHz
+        READ_CYCLES=> 2 --50MHz
+    )
+    port map(
+        CLK=> CLK,
+        WRE_TICK=> WRE_TICK,
+        RE_TICK=> RE_TICK
+    );
+    --CLOCK : 100 MHz clock
     clkA_proc: process
     begin
         loop 
@@ -31,16 +42,6 @@ begin
             wait for 5ns;
             CLK_A<= '1';
             wait for 5ns;
-        end loop;
-    end process;
-    --CLOCK B: 50 MHz clock
-    clkB_proc: process
-    begin
-        loop 
-            CLK_B<='0';
-            wait for 10ns;
-            CLK_B<= '1';
-            wait for 10ns;
         end loop;
     end process;
     stim_proc: process
